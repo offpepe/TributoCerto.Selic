774f57692ff4b0e663612ce97d6b0bf04306a555
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2023-05-15 04:31:34 -0300

    chore: endpoints

---
+++ TributoCerto.SelicJob/Controllers/SelicController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TributoCerto.Selic.Dtos;
+using TributoCerto.Selic.Service.Interfaces;
+
+namespace TributoCerto.Selic.Controllers;
+
+[Route("[controller]")]
+public class SelicController : ControllerBase
+{
+    private readonly ISelicService _service;
+
+    public SelicController(ISelicService service)
+    {
+        _service = service;
+    }
+    [HttpGet]
+    public async Task<IList<SelicAccumulatedDto>> GetAllTaxes()
+    {
+        return await _service.SearchSelicTax();
+    }
+    [HttpPut("[action]")]
+    public async Task UpdateSelicTaxes()
+    {
+        await _service.SeedDatabaseWithSelicTaxes(DateTime.Now.Year - 2013);
+    }
+}
